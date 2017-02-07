fb49b3bd7b9ead719400c1e1ba02f251fe294e8a
pff <pff@qq.com>
2017-02-07 17:20:18 +0800

    新建本地git仓库

--- app/src/main/java/com/feifanzhixing/buglydemo/MainActivity.java
+++ app/src/main/java/com/feifanzhixing/buglydemo/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("bugly","热更新！！！");
+        Log.e("bugly","热更新！！!888888888888888888");
     }
 }
